5121ca9a5a8d784ca9c793d5865f67269ab1360c
fabiohc <fabiohcampos09@gmail.com>
2019-05-28 23:32:43 -0300

    Commit Inicial

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,44 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    AppBar appBar = AppBar(
+      title: Text("Lista de Tarefas"),
+      backgroundColor: Colors.black26,
+      centerTitle: true,
+    );
+
+    TextFormField textFormField = TextFormField(
+      decoration: InputDecoration(
+          labelText: "Nova Tarefa", labelStyle: TextStyle(color: Colors.black)),
+    );
+
+    RaisedButton btnAdd = RaisedButton(onPressed: null,
+    color: Colors.black,
+    child: Text("ADD"),
+      textColor: Colors.white,
+    );
+
+    Row row = Row(
+      children: <Widget>[
+        Expanded(
+       child: textFormField,
+        ),
+        btnAdd
+      ],
+    );
+
+    Container containerTop = Container(
+      padding: EdgeInsets.fromLTRB(18.0, 1.0, 6.0, 1.0),
+      child: row,
+    );
+
+    Column column = Column(
+      children: <Widget>[containerTop],
+    );
+
+    Scaffold scaffold = Scaffold(
+      appBar: appBar,
+    );
+    return scaffold;
   }
 }
